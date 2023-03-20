286b9da25f422567dade473db357b535ff81b728
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-03-20 17:42:19 +0700

    3rd: set_token_encode_decode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  def encode_token(payload)
+    payload[:exp] =  30.days.after.to_i
+    JWT.encode(payload, 'thaogibi')
+  end
+
+  def decode_token
+    auth_header = request.headers['Authorization']
+
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'thaogibi', true, alg: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end  
 end
